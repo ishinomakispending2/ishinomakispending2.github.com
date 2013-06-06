5e55368409a63f42b3953239eb8d5ec94f633e5c
takoratta <takoratta@gmail.com>
2013-06-06 22:11:40 +0900

    Resolved name conflict

--- js/setting.js
+++ js/setting.js
@@ -11,7 +11,7 @@ Taxes.taxRate = 0.06; // 住民税率
 var OpenSpending = OpenSpending || {};
 
 /* OpenSpending.identifier = 'ishinomakinormal'; */
-OpenSpending.identifier = dataset;
+OpenSpending.identifier = datasetid;
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
