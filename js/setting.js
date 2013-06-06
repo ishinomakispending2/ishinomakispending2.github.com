944763b623fa76cd38d3005c4077cc14cbec0b33
takoratta <takoratta@gmail.com>
2013-06-06 22:07:28 +0900

    Added dataset select

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'ishinomakinormal';
+/* OpenSpending.identifier = 'ishinomakinormal'; */
+OpenSpending.identifier = dataset;
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
