91e14ac46997a26b57bbf1de0248e71eb3f50030
ishinomakispending2 <takoratta+ishinomaki2@gmail.com>
2013-06-23 02:24:38 +0900

    identifier to be set "ishinomaki"

--- js/setting.js
+++ js/setting.js
@@ -11,7 +11,7 @@ Taxes.taxRate = 0.06; // 住民税率
 // var OpenSpending = OpenSpending || {};
 OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'ishinomakinormal';
+OpenSpending.identifier = 'ishinomaki';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
@@ -25,10 +25,10 @@ OpenSpending.Styles.Cofog = {
   '2': { icon: 'icons/police2.svg', color: '#C75746', bcolor: '#0AB971' }, // 安全
   '201': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#0AB971' }, // 消防・交通安全
   '202': { icon: 'icons/c_bousai.svg', color: '#C75746', bcolor: '#0AB971' }, // 防災
-  '3': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#4E6D00' }, // まちづくり
+  '3': { icon: 'icons/machi.svg', color: '#C75746', bcolor: '#4E6D00' }, // まちづくり
   '301': { icon: 'icons/EC.svg', color: '#C75746', bcolor: '#4E6D00' }, // 新設
   '302': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#4E6D00' }, // 維持・管理
-  '4': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#D33673' }, // 産業
+  '4': { icon: 'icons/sangyou3.svg', color: '#C75746', bcolor: '#D33673' }, // 産業
   '401': { icon: 'icons/c_nougyou.svg', color: '#C75746', bcolor: '#D33673' }, // 農業
   '402': { icon: 'icons/forest.svg', color: '#C75746', bcolor: '#D33673' }, // 林業
   '403': { icon: 'icons/fishing.svg', color: '#C75746', bcolor: '#D33673' }, // 水産業
@@ -41,7 +41,7 @@ OpenSpending.Styles.Cofog = {
   '6': { icon: 'icons/economic-aid.svg', color: '#C75746', bcolor: '#EC2406' }, // 公債
   '601': { icon: 'icons/economic-aid.svg', color: '#C75746', bcolor: '#EC2406' }, // 公債
   '7': { icon: 'icons/financial-admin.svg', color: '#C75746', bcolor: '#938626' }, // その他
-  '701': { icon: 'icons/courts.svg', color: '#C75746', bcolor: '#938626' }, // 選挙
+  '701': { icon: 'icons/vote01.svg', color: '#C75746', bcolor: '#938626' }, // 選挙
   '702': { icon: 'icons/government-uk.svg', color: '#C75746', bcolor: '#938626' }, // 議会
   '703': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#938626' }, // その他
   '8': { icon: 'icons/aid-developing-countries.svg', color: '#C75746', bcolor: '#C75746' }, // 震災関係
@@ -51,47 +51,6 @@ OpenSpending.Styles.Cofog = {
   '804': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#C75746' }, // 産業
   '805': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#C75746' }, // 健康・福祉・環境
   '806': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#C35B4B' }  // 基金・諸経費
-};
-
-if (window.location.pathname == "/")
-{
-  function dailybreaddraw() {
-
-    (function ($) {
-
-      $(function () {
-        $('#preloader .txt').html('loading data');
-
-       var db = new OpenSpending.DailyBread($('#dailybread'));
-       new OpenSpending.Aggregator({
-           apiUrl: 'http://openspending.org/api',
-           //localApiCache: 'aggregate.json',
-           dataset: OpenSpending.identifier,
-           drilldowns: ['category', 'subcategory'],
-           cuts: ['year:' + OpenSpending.year],
-           rootNodeLabel: 'Total',
-           breakdown: 'category',
-           callback: function(data) {
-
-            $('#content-wrap').show();
-            $('#preloader').remove();
-
-            db.setDataFromAggregator(data, ['unknown']);
-            db.setIconLookup(function(name) {
-              var style = OpenSpending.Styles.Cofog[name];
-              if (style != undefined) {
-               return style['icon'];
-              }
-              return 'icons/unknown.svg';
-            });
-            db.draw();
-           }
-        });
-
-        OpenSpending.renderDependentTypes(db);
-      });
-    })(jQuery)
-  }
 };
 
 if (window.location.pathname == "/bubbletree.html") {
